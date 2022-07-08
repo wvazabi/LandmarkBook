3999871999710a584a2ef8b69a82b48d2012048d
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-08 19:17:37 +0300

    TableView

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Cell Test"
+            return cell
+            
+            
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+  
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+   
+ 
 
 
 }
